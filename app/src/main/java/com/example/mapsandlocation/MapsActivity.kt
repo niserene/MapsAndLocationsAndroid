075b8f3c66ca7fb5df3167d503ae7e423f1c38fd
nishantsahu961 <nishantsahu961@gmail.com>
2021-08-23 19:06:05 +0530

    added the fused location

--- app/src/main/java/com/example/mapsandlocation/MapsActivity.kt
+++ app/src/main/java/com/example/mapsandlocation/MapsActivity.kt
@@ -10,9 +10,11 @@ import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -75,21 +77,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     @SuppressLint("MissingPermission")
     private fun setupLocationListener(){
 
-        val providers:MutableList<String> = locationManager.getProviders(true)
+        val fusedLocationProviderClient:FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
 
-        var loc: Location?=null
-        for(i:Int in providers.indices.reversed()){
-            loc = locationManager.getLastKnownLocation(providers[i])
-            if(loc!=null)
-                break
-        }
-        loc?.let{
-            if(::mMap.isInitialized){
-                val myLoc = LatLng(it.latitude, it.longitude)
-                mMap.addMarker(MarkerOptions().position(myLoc).title("my location"))
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc))
-            }
-        }
+        val locationRequest = LocationRequest()
+            .setInterval(2000)
+            .setFastestInterval(2000)
+            .setSmallestDisplacement(1f)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, object : LocationCallback(){
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for(location in locationResult.locations){
+                        val current = LatLng(location.latitude, location.longitude)
+                        if(::mMap.isInitialized){
+//                            val current = LatLng(23.2599, 77.4126)
+                            mMap.addMarker(MarkerOptions().position(current).title("My location"))
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(current))
+                        }
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
     }
 
     fun isFineLocationGranted():Boolean{
